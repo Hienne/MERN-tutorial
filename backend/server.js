c00cec095a6395d19ff26af544195e01c304817d
hien dep trai <nguyenvanhiencntt3@gmail.com>
2022-12-06 11:26:24 +0700

    Goals controller and route setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const port = process.env.PORT;
+
+const app = express();
+
+// setup middleware cần dùng
+app.use(express.json());    // parse body thành object json
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/goals', require('./routes/goalRoute'));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server start at on port: ${port}`));
